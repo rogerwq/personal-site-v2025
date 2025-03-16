3335bc3f7a2e3c40e6ddf8db3480e300fda4800c
Qin Wan <rogerwq@gmail.com>
2025-03-16 12:18:30 +0900

    giscus

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -63,11 +63,15 @@ const siteMetadata = {
     giscusConfig: {
       // Visit the link below, and follow the steps in the 'configuration' section
       // https://giscus.app/
-      repo: process.env.NEXT_PUBLIC_GISCUS_REPO,
-      repositoryId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID,
-      category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY,
-      categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID,
-      mapping: 'pathname', // supported options: pathname, url, title
+      // repo: process.env.NEXT_PUBLIC_GISCUS_REPO,
+      repo: "rogerwq/personal-site-v2025",
+      // repositoryId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID,
+      repositoryId: "R_kgDONkfYvA",
+      // category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY,
+      category: "Announcements",
+      // categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID,
+      categoryId: "DIC_kwDONkfYvM4CoFl8",
+      mapping: 'url', // supported options: pathname, url, title
       reactions: '1', // Emoji reactions: 1 = enable / 0 = disable
       // Send discussion metadata periodically to the parent window: 1 = enable / 0 = disable
       metadata: '0',
